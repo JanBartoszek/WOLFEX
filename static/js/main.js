4e56ea3063bf9ce65ee66a26d7d399e079c557c1
Jan <bartoszek.jan@gmail.com>
2018-08-23 13:38:57 +0200

    random spawn added (4 starting points)

--- static/js/main.js
+++ static/js/main.js
@@ -16,8 +16,13 @@ let mainDOM = {
     createSoldier : function() {
         let soldier = document.createElement('img')
         soldier.setAttribute('class', 'soldier')
-        soldier.setAttribute('id', data.soldierId)
+        soldier.setAttribute('id', 'soldier' + data.soldierId)
         soldier.setAttribute('src', 'static/img/soldier1.png')
+        let randomIndex = Math.floor(Math.random() * 4);
+        soldier.style.position='absolute';
+        console.log(randomIndex)
+        soldier.style.left = data.spawn[randomIndex].x;
+        soldier.style.top = data.spawn[randomIndex].y;
         document.getElementById('main-container').appendChild(soldier)
         data.soldierId = data.soldierId + 1;
     },
