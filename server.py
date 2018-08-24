2a1fd6c1aaeb4823ebf9d54ae7f19fb3b13665a0
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2018-08-24 10:33:21 +0200

    presentation part added: /presentation, presentation.html, presentation.css"

--- server.py
+++ server.py
@@ -9,5 +9,10 @@ def main():
     return render_template('main.html')
 
 
+@app.route('/presentation')
+def presentation():
+    return render_template('presentation.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
